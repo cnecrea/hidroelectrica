b6379e1ba24961f4707e2419cc30da180651fb97
Ciprian Nicolae <48139709+cnecrea@users.noreply.github.com>
2025-01-13 19:31:44 +0200

    Update const.py
    Refactorizare pentru versiunea 1.1.0: implementare API și coordinator standardizat, conform standardelor Home Assistant

--- custom_components/hidroelectrica/const.py
+++ custom_components/hidroelectrica/const.py
@@ -2,14 +2,18 @@
 
 DOMAIN = "hidroelectrica"
 
-# URL-uri API
+# Adresa de bază
 API_BASE_URL = "https://hidroelectrica-svc.smartcmobile.com"
+
+# Endpoint-uri
 API_URL_GET_ID = f"{API_BASE_URL}/API/UserLogin/GetId"
 API_URL_VALIDATE_LOGIN = f"{API_BASE_URL}/API/UserLogin/ValidateUserLogin"
 API_URL_GET_USER_SETTING = f"{API_BASE_URL}/API/UserLogin/GetUserSetting"
+API_GET_MULTI_METER = f"{API_BASE_URL}/Service/Usage/GetMultiMeter"
+API_GET_MULTI_METER_CURRENT = f"{API_BASE_URL}/Service/SelfMeterReading/GetMeterValue"
+API_GET_MULTI_METER_READ_DATE = f"{API_BASE_URL}/Service/SelfMeterReading/GetWindowDatesENC"
 API_URL_GET_BILL = f"{API_BASE_URL}/Service/Billing/GetBill"
 API_URL_GET_BILL_HISTORY = f"{API_BASE_URL}/Service/Billing/GetBillingHistoryList"
-API_URL_GET_MULTI_METER = f"{API_BASE_URL}/Service/Usage/GetMultiMeter"
 API_URL_GET_USAGE_GENERATION = f"{API_BASE_URL}/Service/Usage/GetUsageGeneration"
 
 # Valori implicite
